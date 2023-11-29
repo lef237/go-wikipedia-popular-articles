b5c46ee3eb02aee161acea48b200cc0f6a641f25
lef237 <lef237k5@gmail.com>
2023-11-29 21:37:53 +0900

    コマンドライン引数を受け取る

---
+++ README.md
@@ -0,0 +1,14 @@
+# Go Wikipedia Popular Articles
+## Description
+This is a simple CLI tool that fetches the most popular articles on Wikipedia and displays them in the terminal.
+
+## How to use
+You can get the most popular articles on Wikipedia in Japan today.
+```
+go run main.go
+```
+
+When you want to get the past articles. You have to format the date as "YYYY-MM-DD".
+```
+go run main.go -date "2020-01-01"
+```
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+type ApiResponse struct {
+	// TODO
+}
+
+func fetchPopularArticles(date string) (*ApiResponse, error) {
+	// TODO
+	return nil, nil
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	// コマンドライン引数の処理
+	dateFlag := flag.String("date", time.Now().Format("2006-01-02"), "Specify the date in YYYY-MM-DD format")
+	flag.Parse()
+
+	// ここまでの結果を出力
+	fmt.Println(*dateFlag)
 }
